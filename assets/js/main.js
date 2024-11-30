222b56d08a5c59027983d44b4e5a6db2aa93ee1a
juel <juelcse2019@gmail.com>
2024-11-30 11:24:50 +0600

    forex code upload

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+jQuery(document).ready(function ($) {
+    // menu bg add
+    function checkScroll() {
+      if ($(window).scrollTop() > 50) {
+          $(".header-area").addClass("menu-bg");
+      } else {
+          $(".header-area").removeClass("menu-bg");
+      }
+    }
+ 
+    $(document).on("click", function (event) {
+      const $div = $("#outside-click");
+      const $navbarText = $("#navbarText");
+      if (!$div.is(event.target) && $div.has(event.target).length === 0) {
+        $navbarText.removeClass("show");
+      }
+    });
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 300) {
+          $('header').addClass('menu-bg-fixed');
+      } else {
+          $('header').removeClass('menu-bg-fixed');
+      }
+    });
+    // Run on page load
+    $(document).ready(function () {
+      checkScroll();
+    });
+
+    // Run on scroll
+    $(window).on("scroll", function () {
+      checkScroll();
+    });
+
+})
